ab6f8d2624eb2aabe079791d589970d795c5e6c3
rainboowbee <rainboowbee@gmail.com>
2024-03-14 18:49:34 +0300

    error correction

---
+++ console/migrations/m240314_153809_create_post_category_id_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post_category_id}}`.
+ */
+class m240314_153809_create_post_category_id_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post_category_id}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%post_category_id}}');
+    }
+}
